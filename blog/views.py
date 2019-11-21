f257f2bf0748b434daf4347034a2e800d51e801c
hyeonhee24 <pkop20@naver.com>
2019-11-21 17:41:19 +0900

    블로그까지 거의 완성

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def allblogs(request):
+	blogs = Blog.objects
+	return render(request,'allblogs.html',{'blogs' : blogs})
